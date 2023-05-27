71e68fc5e8d9c40e07b10694739b176840874622
Douglas <douglasb008@gmail.com>
2023-05-26 21:33:33 -0300

    listar as todos com requisição

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { GlobalStyles } from "@ui/theme/GlobalStyles";
+import { json } from "stream/consumers";
+// import { todoController } from "@server/controller/todo";
+import { todoController } from "@ui/controller/todo";
 
 // const bg = "https://mariosouto.com/cursos/crudcomqualidade/bg";
 
 const bg = "/bg.jpg";
 
+interface HomeTodo {
+  id: string;
+  content: string;
+}
+
 function HomePage() {
+  const [todos, setTodos] = React.useState<HomeTodo[]>([]);
+
+  React.useEffect(() => {
+    todoController.get().then((todos) => {
+      setTodos(todos);
+    });
+  }, []);
+
   return (
     <main>
       <GlobalStyles themeName="indigo" />
@@ -43,51 +59,48 @@ function HomePage() {
           </thead>
 
           <tbody>
-            <tr>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>d4f26</td>
-              <td>
-                Conteúdo de uma TODO Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque vero facilis obcaecati, autem aliquid
-                eius! Consequatur eaque doloribus laudantium soluta optio odit,
-                provident, ab voluptates doloremque voluptas recusandae
-                aspernatur aperiam.
-              </td>
-              <td align="right">
-                <button data-type="delete">Apagar</button>
-              </td>
-            </tr>
+            {todos.map((todo) => {
+              return (
+                <tr key={todo.id}>
+                  <td>
+                    <input type="checkbox" />
+                  </td>
+                  <td>{todo.id.substring(0, 4)}</td>
+                  <td>{todo.content}</td>
+                  <td align="right">
+                    <button data-type="delete">Apagar</button>
+                  </td>
+                </tr>
+                // <tr>
+                //   <td colSpan={4} align="center" style={{ textAlign: "center" }}>
+                //     Carregando...
+                //   </td>
+                // </tr>
 
-            <tr>
-              <td colSpan={4} align="center" style={{ textAlign: "center" }}>
-                Carregando...
-              </td>
-            </tr>
+                // <tr>
+                //   <td colSpan={4} align="center">
+                //     Nenhum item encontrado
+                //   </td>
+                // </tr>
 
-            <tr>
-              <td colSpan={4} align="center">
-                Nenhum item encontrado
-              </td>
-            </tr>
-
-            <tr>
-              <td colSpan={4} align="center" style={{ textAlign: "center" }}>
-                <button data-type="load-more">
-                  Carregar mais{" "}
-                  <span
-                    style={{
-                      display: "inline-block",
-                      marginLeft: "4px",
-                      fontSize: "1.2em",
-                    }}
-                  >
-                    ↓
-                  </span>
-                </button>
-              </td>
-            </tr>
+                // <tr>
+                //   <td colSpan={4} align="center" style={{ textAlign: "center" }}>
+                //     <button data-type="load-more">
+                //       Carregar mais{" "}
+                //       <span
+                //         style={{
+                //           display: "inline-block",
+                //           marginLeft: "4px",
+                //           fontSize: "1.2em",
+                //         }}
+                //       >
+                //         ↓
+                //       </span>
+                //     </button>
+                //   </td>
+                // </tr>
+              );
+            })}
           </tbody>
         </table>
       </section>
